eb6bacb3ff747b73a66f33eb33a15086dc94b23d
Mark S. and Matt <mark-schultz@pluralsight.com, matt-mcmurry@pluralsight.com>
2015-07-30 11:14:48 -0600

    Create an Xcode project

---
+++ PSOperations/PSOperations/PSOperations.h
@@ -0,0 +1,19 @@
+//
+//  PSOperations.h
+//  PSOperations
+//
+//  Created by Dev Team on 7/30/15.
+//  Copyright (c) 2015 Pluralsight. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PSOperations.
+FOUNDATION_EXPORT double PSOperationsVersionNumber;
+
+//! Project version string for PSOperations.
+FOUNDATION_EXPORT const unsigned char PSOperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PSOperations/PublicHeader.h>
+
+
