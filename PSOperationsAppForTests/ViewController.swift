5ae7e0fc40729407ec63a233cdec8078283e3bd8
Matt McMurry <mattmcmurry@gmail.com>
2016-02-02 15:45:57 -0700

    Fix case where dependencies cause crash when all associated operations are completed

---
+++ PSOperationsAppForTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PSOperationsAppForTests
+//
+//  Created by Matt McMurry on 1/11/16.
+//  Copyright © 2016 Pluralsight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
