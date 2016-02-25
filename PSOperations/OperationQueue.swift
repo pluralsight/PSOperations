b9da72d901f6303d2d5be704dd541b78d3f08bf8
Mark S. and Matt <mark-schultz@pluralsight.com, matt-mcmurry@pluralsight.com>
2016-02-25 14:49:50 -0700

    Serialize the adding and removing of operations from the ops set to fix... potential threading issues

--- PSOperations/OperationQueue.swift
+++ PSOperations/OperationQueue.swift
@@ -32,11 +32,13 @@ import Foundation
     - Setting up dependencies to enforce mutual exclusivity
 */
 public class OperationQueue: NSOperationQueue {
+    private let opsQueue = dispatch_queue_create("com.psoperations", DISPATCH_QUEUE_SERIAL)
     private var ops: Set<NSOperation> = Set()
+    
     public weak var delegate: OperationQueueDelegate?
     
     override public  func addOperation(operation: NSOperation) {
-        ops.insert(operation)
+        addOpToSet(operation)
         if let op = operation as? Operation {
             
             // Set up a `BlockObserver` to invoke the `OperationQueueDelegate` method.
@@ -48,7 +50,7 @@ public class OperationQueue: NSOperationQueue {
                 finishHandler: { [weak self] in
                     if let q = self {
                         q.delegate?.operationQueue?(q, operationDidFinish: $0, withErrors: $1)
-                        q.ops.remove($0)
+                        q.removeOpFromSet($0)
                     }
                 }
             )
@@ -96,7 +98,7 @@ public class OperationQueue: NSOperationQueue {
             operation.addCompletionBlock { [weak self, weak operation] in
                 guard let queue = self, let operation = operation else { return }
                 queue.delegate?.operationQueue?(queue, operationDidFinish: operation, withErrors: [])
-                queue.ops.remove(operation)
+                queue.removeOpFromSet(operation)
             }
         }
         
@@ -126,6 +128,18 @@ public class OperationQueue: NSOperationQueue {
             for operation in ops {
               operation.waitUntilFinished()
             }
+        }
+    }
+    
+    func addOpToSet(op: NSOperation) {
+        dispatch_async(opsQueue) {
+            self.ops.insert(op)
+        }
+    }
+    
+    func removeOpFromSet(op: NSOperation) {
+        dispatch_async(opsQueue) {
+            self.ops.remove(op)
         }
     }
 }
