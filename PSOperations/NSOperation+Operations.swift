17de4b9ca336ac16bb6e01bbe26d6cfa415d6432
mwgray <mwgray@users.noreply.github.com>
2016-08-15 10:34:14 -0700

    Update NSOperation+Operations.swift
    publicize extensions

--- PSOperations/NSOperation+Operations.swift
+++ PSOperations/NSOperation+Operations.swift
@@ -13,7 +13,7 @@ extension NSOperation {
         Add a completion block to be executed after the `NSOperation` enters the
         "finished" state.
     */
-    func addCompletionBlock(block: Void -> Void) {
+    public func addCompletionBlock(block: Void -> Void) {
         if let existing = completionBlock {
             /*
                 If we already have a completion block, we construct a new one by
@@ -30,7 +30,7 @@ extension NSOperation {
     }
 
     /// Add multiple depdendencies to the operation.
-    func addDependencies(dependencies: [NSOperation]) {
+    public func addDependencies(dependencies: [NSOperation]) {
         for dependency in dependencies {
             addDependency(dependency)
         }
