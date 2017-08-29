4572a9b29fe5aefb685ecc38153fdf00359e6875
Matt <mattmcmurry@gmail.com>
2017-08-29 11:43:06 -0600

    Move State out of Operation

--- PSOperations/Operation.swift
+++ PSOperations/Operation.swift
@@ -49,58 +49,6 @@ open class Operation: Foundation.Operation {
     
     // MARK: State Management
     
-    fileprivate enum State: Int, Comparable {
-        /// The initial state of an `Operation`.
-        case initialized
-        
-        /// The `Operation` is ready to begin evaluating conditions.
-        case pending
-        
-        /// The `Operation` is evaluating conditions.
-        case evaluatingConditions
-        
-        /**
-            The `Operation`'s conditions have all been satisfied, and it is ready 
-            to execute.
-        */
-        case ready
-        
-        /// The `Operation` is executing.
-        case executing
-        
-        /**
-            Execution of the `Operation` has finished, but it has not yet notified 
-            the queue of this.
-        */
-        case finishing
-        
-        /// The `Operation` has finished executing.
-        case finished
-        
-        func canTransitionToState(_ target: State, operationIsCancelled cancelled: Bool) -> Bool {
-            switch (self, target) {
-            case (.initialized, .pending):
-                return true
-            case (.pending, .evaluatingConditions):
-                return true
-            case (.pending, .finishing) where cancelled:
-                return true
-            case (.pending, .ready) where cancelled:
-                return true
-            case (.evaluatingConditions, .ready):
-                return true
-            case (.ready, .executing):
-                return true
-            case (.ready, .finishing):
-                return true
-            case (.executing, .finishing):
-                return true
-            case (.finishing, .finished):
-                return true
-            default:
-                return false
-            }
-        }
     }
     
     /**
