dfb3bbc6b387a382455a770b8486acbc015a19c6
Florian Friedrich <ffried@me.com>
2020-10-22 16:33:53 +0200

    Modernize two extension implementations

--- PSOperations/NSLock+Operations.swift
+++ PSOperations/NSLock+Operations.swift
@@ -11,8 +11,7 @@ import Foundation
 extension NSLock {
     func withCriticalScope<T>(_ block: () -> T) -> T {
         lock()
-        let value = block()
-        unlock()
-        return value
+        defer { unlock() }
+        return block()
     }
 }
